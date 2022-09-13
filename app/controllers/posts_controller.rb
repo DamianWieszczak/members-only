8432451668ab244ca63b5c6981471077aff83e7c
DamianWieszczak <damian.wieszczak@gmail.com>
2022-09-13 23:53:29 +0200

    Add posts/users views, add migration, edit controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,30 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @posts = Post.all
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }  
+  validates :username, uniqueness: true, length: { maximum: 30 }  
+  has_many :posts
 end
