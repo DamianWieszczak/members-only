e171ab799dcf4a82228531a479beec481ea33d8a
DamianWieszczak <damian.wieszczak@gmail.com>
2022-09-13 21:59:49 +0200

    Create Posts Controller, add simple form, add new controller for turbo/devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }  
+  validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }  
 end
